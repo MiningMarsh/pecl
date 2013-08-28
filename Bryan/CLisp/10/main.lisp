ccc7ecbc151fb7a0831c2a143a5569a86ee02974
Bryan Hoyle <starfoxprime@gmail.com>
2013-08-28 08:07:07 -0400

    project 10, done

--- Bryan/CLisp/10/main.lisp
+++ Bryan/CLisp/10/main.lisp
@@ -3,4 +3,4 @@
 (setq *project-description* "The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.")
 (defun main ()
-  (format t "~a~%" 1 ))
+  (format t "~a~%" (apply #'+ (primes<n 2000000))))
