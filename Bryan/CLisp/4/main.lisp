d91a50db42a5780407f394f7d51befee27a59b97
Bryan Hoyle <starfoxprime@gmail.com>
2013-10-29 11:28:43 -0400

    This is a test commit

--- Bryan/CLisp/4/main.lisp
+++ Bryan/CLisp/4/main.lisp
@@ -4,19 +4,32 @@
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
 ")
-(defun palindrome-number (x)
-  (let ((str (write-to-string x)))
-	(string= str (reverse str))))
+
+(defun palindrome-number (x &optional (sz (log x 10)))
+  (labels
+	  ((rec ( base1 base2)
+		 (cond
+		   ((>= base2 base1) t)
+		   ((not
+			 (= (mod (truncate x base1) 10) (mod (truncate x base2) 10)))
+			nil)
+		   (t
+			(rec (/ base1 10) (* base2 10))))))
+	(rec  (expt 10 (truncate sz)) 1)))
+
+
 (defun crawler (max-num)
-  (labels ((rec (i j upb mp) 
-			 (let ((pr (* i j)))
-			   (cond
-				 ((<= i upb) mp) 
-				 ((< pr mp) (rec (1- i) max-num upb mp))
-				 ((palindrome-number pr) (rec (1- i) max-num (sqrt pr) pr))
-				 ((<= j upb) (rec (1- i) max-num upb mp))
-				 (t (rec i (1- j) upb mp))))))
-    (rec max-num max-num 1 1)))
+  (let ((sz (log (square max-num) 10)))
+	(labels ((rec (i j upb mp) 
+			   (let ((pr (* i j)))
+				 (cond
+				   ((<= i upb) mp) 
+				   ((< pr mp) (rec (1- i) max-num upb mp))
+				   ((palindrome-number pr sz) (rec (1- i) max-num (sqrt pr) pr))
+				   ((<= j upb) (rec (1- i) max-num upb mp))
+				   (t (rec i (1- j) upb mp))))))
+	  (rec max-num max-num 1 1))))
+
 (defun main ()
   (crawler 999))
 
