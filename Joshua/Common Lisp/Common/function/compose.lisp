6a72aff0a647352daba4c1ea958659a2b0f6a4d2
Joshua Songy <joshua.songy@gmail.com>
2014-03-17 19:18:46 +0000

    Added macro.

--- Joshua/Common Lisp/Common/function/compose.lisp
+++ Joshua/Common Lisp/Common/function/compose.lisp
@@ -1,1 +1,15 @@
-(defun compose () 1)
+(defun compose (&rest funcs)
+	(let ((func (car funcs))
+	      (rest (cdr funcs)))
+	(if rest
+		(let ((composed (apply (the function #'compose) rest)))
+			(lambda (&rest args)
+				(funcall
+					(the function func) 
+					(car (funcall 
+						(the function composed) 
+						args)))))
+		(lambda (&rest args) 
+			(apply 
+				(the function func)
+				args)))))
