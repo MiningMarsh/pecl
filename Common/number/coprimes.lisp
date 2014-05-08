e244d6af58d719daaae3d3895426f06b410c34c7
Joshua Songy <joshua.songy@gmail.com>
2014-05-08 15:50:43 -0400

    Moved coprime generator over to its own function.

---
+++ Common/number/coprimes.lisp
@@ -0,0 +1,12 @@
+(defun coprimes (max)
+"Returns a list of coprime pairs (n m) where n < m, and every m < max."
+	(labels ((recurse (acc n m)
+			(if (> m max)
+				acc
+				(cons (list n m)
+					(recurse 
+						(recurse
+							(recurse acc m (+ (* 2 m) n))
+							n (+ m (* 2 n)))
+						m (- (* 2 m) n))))))
+		(recurse (recurse nil 1 2) 1 3)))
--- coprimes.lisp
+++ coprimes.lisp
@@ -1,15 +1,3 @@
-(defun coprimes (max)
-"Returns a list of coprime pairs (n m) where n < m, and every m < max."
-	(labels ((recurse (acc n m)
-			(if (> m max)
-				acc
-				(cons (list n m)
-					(recurse 
-						(recurse
-							(recurse acc m (+ (* 2 m) n))
-							n (+ m (* 2 n)))
-						m (- (* 2 m) n))))))
-		(recurse nil 1 2)))
 
 (defun triplet (n m)
 "Generates a pythagorean triple from given coprimes of different parity."
