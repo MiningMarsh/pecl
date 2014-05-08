4fd15da269628a89c9305861e292657c8aff75fb
Joshua Songy <joshua.songy@gmail.com>
2014-05-08 16:47:45 -0400

    Documented derivative function.

--- Common/function/derivative.lisp
+++ Common/function/derivative.lisp
@@ -1,4 +1,6 @@
 (defun derivative (f &optional (x nil x-defined-p))
+"Returns the derivative of function f, or return the derivative of f at 
+point x."
 	; This delta was hand tweaked to provide accurate results
 	(let*
 			((delta 0.001)
