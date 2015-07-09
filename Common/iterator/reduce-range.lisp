f8caea8f9ebd307be1589b7208ac01ef47ecafee
Joshua Songy <joshua.songy@gmail.com>
2015-07-09 00:12:36 -0400

    Rewrote reduce-range to use more efficient loop macro.

--- Common/iterator/reduce-range.lisp
+++ Common/iterator/reduce-range.lisp
@@ -1,5 +1,16 @@
-(defun reduce-range (fn start end &key (step 1) (initial-value nil initial-value-provided-p))
-	(let ((iterator (iterator-from-range start end step)))
-		(if initial-value-provided-p
-			(reduce-iterator fn iterator :initial-value initial-value)
-			(reduce-iterator fn iterator))))
+(defun reduce-range (fn start end &key (s 1) (initial-value nil initial-value-provided-p))
+	(macrolet
+			((loop-range (stepper direction)
+				`(loop
+					for i
+					from (if initial-value-provided-p start (,stepper start s))
+					,direction end
+					by s
+					do (setq acc (funcall fn acc i)))))
+		(let ((direction (if (< start end) t nil))
+		      (acc (if initial-value-provided-p start)))
+			(if direction
+				(loop-range + to)
+				(loop-range - downto))
+			acc)))
+
